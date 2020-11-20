a64ac85ee4c313ef37d509b31354b222e1365258
BertilleFihey <73773978+BertilleFihey@users.noreply.github.com>
2020-11-20 18:00:14 +0100

    Add files via upload
    I share with you the "capture d'écran" that shows that when I try to knit my Rmd file in PDF, there is written a red message of error.

---
+++ misc.R
@@ -0,0 +1,19 @@
+# This script contains miscellaneous utilities.
+# You will use this script in your challenge 2.
+
+numeric_vector1 <- c(-1, -2, -3, 1, 2)
+
+numeric_vector2 <- c(-4, -5, -6, 3, 4)
+
+numeric_vector3 <- c(-7, -8, -9, 5, 6)
+
+my_list <- list(numeric_vector1, numeric_vector2, numeric_vector3)
+
+negative_mean <- function(numeric_vector1)
+  
+output <-(-1, -2, -3, -4, -5, -6, -7, -8, -9)
+
+return(output)
+
+for(c(numeric_vector1, numeric_vector2, numeric_vector3)){print(negative_mean(numeric_vector1, numeric_vector2, numeric_vector3, c))}
+
